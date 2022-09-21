1c994f2dee7ef7b1b941cb1bbf86b00872820293
bersoncrios <bersondev@gmail.com>
2022-09-20 22:47:56 -0300

    finalizando grosso do site

---
+++ js/index.js
@@ -0,0 +1,3 @@
+var image = document.getElementById("hb");
+image.onmouseover = function() {  image.src = "/Users/bersoncrios/Documents/Repositories/AppsOptheya/lp-optheya/resources/images/hb_aberto.png"; }
+image.onmouseout = function() {  image.src = "/Users/bersoncrios/Documents/Repositories/AppsOptheya/lp-optheya/resources/images/hb_fechado.png"; }
