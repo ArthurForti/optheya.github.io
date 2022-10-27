739dcaa6a0279b331781e4026bc7df7c82fbd40d
bersoncrios <bersondev@gmail.com>
2022-10-27 18:14:49 -0300

    Ajustes de cr

---
+++ javascript/capture-lead.js
@@ -0,0 +1,3 @@
+function capturaLead() {
+    console.log("captura");
+}
--- javascript/partner-form.js
+++ javascript/partner-form.js
@@ -1,10 +1,8 @@
 function isChecked() {
     if(document.getElementById("my-checkbox").checked) {
-        console.log("abriuuuu");
         document.querySelector(".partner-form").classList.remove("hidden");
     }
     else {
-        console.log("fechouuuu");
         document.querySelector(".partner-form").classList.add("hidden");
     }
 }
